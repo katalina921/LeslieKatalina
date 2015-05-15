b3af26246b8dfc5dfa981e1cecaa96fd2843e62b
Leslie Segovia <lesliesegovia1030@gmail.com>
2015-05-15 08:49:02 -0700

    fixed error

--- index.php
+++ index.php
@@ -1,20 +1,6 @@
 <?php
-<<<<<<< HEAD
     require_once ("php/controller/create-db.php");
 ?>
-<!DOCTYPE HTML>
-<html>
-	<head>
-		<title>melonJS Template</title>
-		<link rel="stylesheet" type="text/css" media="screen" href="index.css">
-		<meta id="viewport" name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-		<meta name="apple-mobile-web-app-capable" content="yes">
-		<meta name="mobile-web-app-capable" content="yes">
-		<meta name="apple-mobile-web-app-status-bar-style" content="black">
-=======
-require_once(__DIR__ . "/php/controller/create-db.php");
-?>
-
 <html>
     <head>
         <title>melonJS Template</title>
@@ -23,12 +9,10 @@ require_once(__DIR__ . "/php/controller/create-db.php");
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="mobile-web-app-capable" content="yes">
         <meta name="apple-mobile-web-app-status-bar-style" content="black">
->>>>>>> 7ef944b9fa2a0a41802bfd961c211d14d0cea12d
         <link rel="apple-touch-icon" href="icons/touch-icon-iphone-60x60.png">
         <link rel="apple-touch-icon" sizes="76x76" href="icons/touch-icon-ipad-76x76.png">
         <link rel="apple-touch-icon" sizes="120x120" href="icons/touch-icon-iphone-retina-120x120.png">
         <link rel="apple-touch-icon" sizes="152x152" href="icons/touch-icon-ipad-retina-152x152.png">
-<<<<<<< HEAD
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
         <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
